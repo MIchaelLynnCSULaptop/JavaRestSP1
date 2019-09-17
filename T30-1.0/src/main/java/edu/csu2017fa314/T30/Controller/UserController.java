f39e35403cff7e1124d30506ce9c21a92ba02679
Michael Lynn <hailviral101@yahoo.com>
2019-09-17 01:47:26 -0600

    routes and dynamic varibles
    added routes and dynamic varibles to webpage using VelocityTemplateEngine

--- T30-1.0/src/main/java/edu/csu2017fa314/T30/Controller/UserController.java
+++ T30-1.0/src/main/java/edu/csu2017fa314/T30/Controller/UserController.java
@@ -2,11 +2,16 @@ package edu.csu2017fa314.T30.Controller;
 
 import com.google.gson.Gson;
 import edu.csu2017fa314.T30.Model.Model;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import static spark.Spark.*;
 
@@ -16,6 +21,21 @@ public class UserController {
     int test = 0;
 
     public String callme(Model myusers) {
+
+//        VelocityEngine ve = new VelocityEngine();
+//        Properties props = new Properties();
+//        props.put("file.resource.loader.path", "/Users/aplus/Documents/GitHub/JavaRestSP1/T30-1.0/resources/");
+//        ve.init(props);
+//
+//        Template t = ve.getTemplate("hello.vm");
+//        VelocityContext context = new VelocityContext();
+//        String templateVariable = "Hello Velocity!";
+//        context.put("message",templateVariable);  // put your template values here
+//        StringWriter writer = new StringWriter();
+//        t.merge(context, writer);
+//
+//        System.out.println(writer.toString()); // print the updated template as string
+
         test++;
         System.out.println(test);
         System.out.println("Hello ");
@@ -27,13 +47,21 @@ public class UserController {
 
     public UserController(final Model myusers) {
 
-        get("/users", (req, res) -> {
-                    callme(myusers);
-                    Map<String, Object> model = new HashMap<>();
-                    model.put("hello", "Velocity World");
-                    return new ModelAndView(model, "hello.vt");
-                }, new VelocityTemplateEngine());
+//        // minium default to run
+//        get("/users", (request, response) -> {
+//            return new ModelAndView(new HashMap(), "hello.vm");
+//        }, new VelocityTemplateEngine());
 
+        get("/users", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String templateVariable = "Hello Velocity!";
+            model.put("message", templateVariable);
+            return new ModelAndView(model, "hello.vm");
+
+        }, new VelocityTemplateEngine());
+
+
+        // get("/users", (req, res) -> callme(myusers));
 
         after((req, res) -> {
             res.type("application/json");
